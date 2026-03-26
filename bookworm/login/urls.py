5fa08cce698447c6b8cda1512e37d81fab2ec822
Andres-Garcia-Sal <andres.garciasalinas@my.utsa.edu>
2026-03-26 18:42:26 -0500

    adjusted change for .gitignore. Just commented a 2 things out

--- bookworm/login/urls.py
+++ bookworm/login/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [ path('function', views.hello_world),
-                path('class', views.HelloWorldView.as_view()),
+urlpatterns = [ path('login', views.login_view, name='login'),
+    path('register', views.register_view, name='register'),
+    path('logout', views.logout_view, name='logout')
 ]
--- bookworm/login/views.py
+++ bookworm/login/views.py
@@ -6,9 +6,21 @@ from django.views import View
 # Create your views here.
 #test for connection of front and back end
 
-def hello_world(request):
-    return HttpResponse("Hello, World!")
+def login_view(request):
+    if request.method == 'POST':
+        # Handle login logic here
+        return HttpResponse("Login successful!")
+    else:
+        return render(request, 'login/login.html')
+    
+def register_view(request):
+    if request.method == 'POST':
+        # Handle registration logic here
+        return HttpResponse("Registration successful!")
+    else:
+        return render(request, 'login/register.html')
+    
+def logout_view(request):
+    # Handle logout logic here
+    return HttpResponse("Logout successful!")
 
-class HelloWorldView(View):
-     def get(self, request):
-          return HttpResponse("Hello, World View!")
