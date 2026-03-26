6653b89d0e970db21aded9c1ea0612901687cbf0
Ssolar5 <109261817+Ssolar5@users.noreply.github.com>
2026-03-26 13:35:22 -0500

    feat: Change display names for some models

--- bookworm/catalog/models.py
+++ bookworm/catalog/models.py
@@ -11,6 +11,13 @@ class BookDetails(models.Model):
     author = models.CharField(max_length=255)
     description = models.CharField(max_length=2047)
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Book details"
+        verbose_name_plural = "Book details"
+
 
 class StockBook(models.Model):
     """Copies of a book as a part of the store's stock."""
@@ -18,3 +25,10 @@ class StockBook(models.Model):
     details = models.OneToOneField(BookDetails, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     stock = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"(x{self.stock}) {self.details}"
+
+    class Meta:
+        verbose_name = "Book in stock"
+        verbose_name_plural = "Books in stock"
