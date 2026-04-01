a89eda5f22341bedb08b5087ede76df15aa9cca9
Ssolar5 <109261817+Ssolar5@users.noreply.github.com>
2026-04-01 16:41:17 -0500

    feat(accounts): Add login, register, and logout
    - Also add a few quality things such as root URL redirects to book list.

---
+++ bookworm/accounts/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+
+class ChangePasswordForm(forms.Form):
+    current_password = forms.CharField(widget=forms.PasswordInput)
+    new_password     = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
